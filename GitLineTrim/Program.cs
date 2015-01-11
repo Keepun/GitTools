a3fad3abbcf3b04dd4369db997bc485ae4d9cb6c
Keepun <keepun@gmail.com>
2015-01-11 07:09:58 +0300

    mega fix + test

--- GitLineTrim/Program.cs
+++ GitLineTrim/Program.cs
@@ -8,145 +8,211 @@ namespace GitLineTrim
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             List<string> files = new List<string>();
             bool backup = true;
-            for (int x = 0; x < args.Length; x++)
-            {
+            for (int x = 0; x < args.Length; x++) {
                 int pos = args[x].ToLower().IndexOf("nobackup");
-                if (pos > 0 && args[x].IndexOfAny(new char[] { '-', '/' }, 0, pos) > -1)
-                {
+                if (pos > 0 && args[x].IndexOfAny(new char[] {'-', '/'}, 0, pos) > -1) {
                     backup = false;
-                }
-                else
-                {
+                } else {
                     files.Add(args[x]);
                 }
             }
-            if (files.Count < 1)
-            {
-                Console.WriteLine("No files!");
-                Console.WriteLine("{0} [-nobackup] files",
-                    Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location));
-                return;
-            }
-            foreach (string file in files)
-            {
-                Console.Write(file);
-                if (backup)
-                {
-                    File.Copy(file, file + ".bak");
-                }
-                try
-                {
-                    Encoding codepage;
-                    byte[] bom = new byte[4];
-                    int seekbom = 0;
-                    using (BinaryReader ffrom = new BinaryReader(File.OpenRead(file)))
-                    {
-                        ffrom.BaseStream.Read(bom, 0, bom.Length);
-                        if (bom[0] == 0x00 && bom[1] == 0x00 && bom[2] == 0xFE && bom[3] == 0xFF)
-                        {
-                            codepage = Encoding.GetEncoding("UTF-32BE"); // UTF-32, big-endian
-                            seekbom = 4;
+            try {
+                using (MemoryStream finput = new MemoryStream()) {
+                    using (Stream bcon = Console.OpenStandardInput()) {
+                        byte[] buffer = new byte[100000];
+                        int count;
+                        while ((count = bcon.Read(buffer, 0, buffer.Length)) > 0) {
+                            finput.Write(buffer, 0, count);
                         }
-                        else if (bom[0] == 0xFE && bom[1] == 0xFF)
-                        {
-                            codepage = Encoding.BigEndianUnicode; // UTF-16, big-endian
-                            seekbom = 2;
-                        }
-                        else if (bom[0] == 0xFF && bom[1] == 0xFE)
-                        {
-                            if (bom[2] == 0x00 && bom[2] == 0x00)
-                            {
-                                codepage = Encoding.UTF32; // UTF-32, little-endian
-                                seekbom = 4;
-                            }
-                            else
-                            {
-                                codepage = Encoding.Unicode; // UTF-16, little-endian
-                                seekbom = 2;
-                            }
-                        }
-                        else if (bom[0] == 0xEF && bom[1] == 0xBB && bom[2] == 0xBF)
-                        {
-                            codepage = Encoding.UTF8;
-                            seekbom = 3;
-                        }
-                        else
-                        {
-                            codepage = Encoding.Default;
-                            seekbom = 0;
-
-                            bool binary = false;
-                            long fsize = ffrom.BaseStream.Length;
-                            if (fsize > 100000)
-                            {
-                                fsize = 100000;
-                            }
-                            byte[] bts = new byte[fsize];
-                            ffrom.BaseStream.Seek(seekbom, SeekOrigin.Begin);
-                            ffrom.BaseStream.Read(bts, 0, (int)fsize);
-                            for (int x = 0; x < fsize; x++)
-                            {
-                                if (bts[x] == 0)
-                                {
-                                    binary = true;
-                                    break;
+                    }
+                    try {
+                        if (finput.Length > 0) {
+                            long filesize;
+                            using (MemoryStream fmem = LineTrim(finput, out filesize)) {
+                                if (fmem == null) {
+                                    using (Stream bcon = Console.OpenStandardOutput()) {
+                                        finput.WriteTo(bcon);
+                                    }
+                                    return;
                                 }
-                            }
-                            if (binary)
-                            {
-                                Console.WriteLine(" - Binary");
-                                continue;
+                                using (Stream bcon = Console.OpenStandardOutput()) {
+                                    fmem.WriteTo(bcon);
+                                }
+                                return;
                             }
                         }
                     }
-                    using (MemoryStream fmem = new MemoryStream())
-                    {
-                        using (StreamReader ffrom = new StreamReader(file, codepage))
-                        {
-                            fmem.Write(bom, 0, seekbom);
-                            ffrom.BaseStream.Seek(seekbom, SeekOrigin.Begin);
-                            string line;
-                            while ((line = ffrom.ReadLine()) != null)
-                            {
-                                line = line.TrimEnd() + Environment.NewLine;
-                                byte[] bline = codepage.GetBytes(line);
-                                fmem.Write(bline, 0, bline.Length);
-                            }
-                            if (fmem.Length >= ffrom.BaseStream.Length)
-                            {
-                                continue;
-                            }
+                    catch (Exception ex) {
+                        ToLog(args, ex);
+                        return;
+                    }
+                }
+            }
+            catch {
+            }
+            if (files.Count < 1) {
+                Console.WriteLine("{0} [-nobackup] [files]",
+                                  Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location));
+                return;
+            }
+            foreach (string file in files) {
+                Console.Write(file);
+                try {
+                    long filesize;
+                    using (MemoryStream fmem = LineTrim(File.OpenRead(file), out filesize)) {
+                        if (fmem == null) {
+                            Console.WriteLine(" - Binary");
+                            continue;
                         }
-                        using (Stream fto = File.Open(file, FileMode.Truncate, FileAccess.Write, FileShare.Read))
-                        {
+                        if (fmem.Length == filesize) {
+                            continue;
+                        }
+                        if (backup) {
+                            File.Copy(file, file + ".bak", true);
+                        }
+                        using (Stream fto = File.Open(file, FileMode.Truncate, FileAccess.Write, FileShare.Read)) {
                             fmem.WriteTo(fto);
                         }
                     }
                     Console.WriteLine(" - OK");
                 }
-                catch (Exception ex)
-                {
+                catch (Exception ex) {
                     Console.WriteLine();
                     Console.WriteLine(ex);
-                    using (StreamWriter flog = new StreamWriter(
-                        "!" + Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location) + ".log", true))
-                    {
-                        if (flog.BaseStream.Position > 0)
-                        {
-                            flog.WriteLine();
+                    ToLog(args, ex);
+                }
+            }
+        }
+
+        private static MemoryStream LineTrim(Stream stream, out long fileSize)
+        {
+            fileSize = stream.Length;
+            byte[] bom;
+            int seekbom;
+            Encoding codepage = GetEncodingStream(stream, out bom, out seekbom);
+            if (codepage == null) {
+                return null;
+            }
+            MemoryStream fmem = new MemoryStream();
+            using (StreamReader ffrom = new StreamReader(stream, codepage)) {
+                fmem.Write(bom, 0, seekbom);
+                ffrom.DiscardBufferedData();
+                ffrom.BaseStream.Seek(seekbom, SeekOrigin.Begin);
+                string newLine = "\r\n";
+                string line;
+                if ((line = ffrom.ReadLine()) != null) {
+                    ffrom.DiscardBufferedData();
+                    ffrom.BaseStream.Seek(seekbom + codepage.GetByteCount(line), SeekOrigin.Begin);
+                    int ch = ffrom.Peek();
+                    if (ch != -1) {
+                        if (ch == '\n') {
+                            newLine = "\n";
+                            ffrom.Read();
+                        } else if (ch == '\r') {
+                            ffrom.Read();
+                            if ((ch = ffrom.Peek()) != -1 && ch == '\n') {
+                                newLine = "\r\n";
+                                ffrom.Read();
+                            } else {
+                                newLine = "\r";
+                            }
                         }
-                        flog.WriteLine(DateTime.Now);
-                        foreach (string arg in args)
-                        {
-                            flog.WriteLine(arg);
+                    }
+                    do {
+                        line = line.TrimEnd();
+                        if (ffrom.EndOfStream) {
+                            int cnl = codepage.GetByteCount(newLine);
+                            if (ffrom.BaseStream.Length >= cnl) {
+                                ffrom.DiscardBufferedData();
+                                ffrom.BaseStream.Seek(-1*cnl, SeekOrigin.End);
+                                bool bnl = true;
+                                foreach (char chr in newLine) {
+                                    if (chr != ffrom.Read()) {
+                                        bnl = false;
+                                    }
+                                }
+                                if (bnl) {
+                                    line += newLine;
+                                }
+                            }
+                        } else {
+                            line += newLine;
                         }
-                        flog.WriteLine(ex);
+                        byte[] bline = codepage.GetBytes(line);
+                        fmem.Write(bline, 0, bline.Length);
+                    } while ((line = ffrom.ReadLine()) != null);
+                }
+            }
+            fmem.Seek(0, SeekOrigin.Begin);
+            return fmem;
+        }
+
+        public static Encoding GetEncodingStream(Stream stream, out byte[] bom, out int sizebom)
+        {
+            BinaryReader bin = new BinaryReader(stream);
+            bom = new byte[4];
+            bin.BaseStream.Read(bom, 0, bom.Length);
+            bin.BaseStream.Seek(0, SeekOrigin.Begin);
+            if (bom[0] == 0x00 && bom[1] == 0x00 && bom[2] == 0xFE && bom[3] == 0xFF) {
+                sizebom = 4;
+                return Encoding.GetEncoding("UTF-32BE"); // UTF-32, big-endian
+            } else if (bom[0] == 0xFE && bom[1] == 0xFF) {
+                sizebom = 2;
+                return Encoding.BigEndianUnicode; // UTF-16, big-endian
+            } else if (bom[0] == 0xFF && bom[1] == 0xFE) {
+                if (bom[2] == 0x00 && bom[2] == 0x00) {
+                    sizebom = 4;
+                    return Encoding.UTF32; // UTF-32, little-endian
+                } else {
+                    sizebom = 2;
+                    return Encoding.Unicode; // UTF-16, little-endian
+                }
+            } else if (bom[0] == 0xEF && bom[1] == 0xBB && bom[2] == 0xBF) {
+                sizebom = 3;
+                return Encoding.UTF8;
+            } else {
+                sizebom = 0;
+
+                bool binary = false;
+                long fsize = bin.BaseStream.Length;
+                if (fsize > 100000) {
+                    fsize = 100000;
+                }
+                byte[] bts = new byte[fsize];
+                bin.BaseStream.Seek(sizebom, SeekOrigin.Begin);
+                bin.BaseStream.Read(bts, 0, (int)fsize);
+                bin.BaseStream.Seek(0, SeekOrigin.Begin);
+                for (int x = 0; x < fsize; x++) {
+                    if (bts[x] == 0) {
+                        binary = true;
+                        break;
                     }
                 }
+                if (binary) {
+                    return null;
+                }
+
+                return Encoding.Default;
+            }
+        }
+
+        private static void ToLog(string[] args, Exception ex)
+        {
+            using (StreamWriter flog = new StreamWriter(
+                "!" + Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location) + ".log", true)
+                ) {
+                if (flog.BaseStream.Position > 0) {
+                    flog.WriteLine();
+                }
+                flog.WriteLine(DateTime.Now);
+                foreach (string arg in args) {
+                    flog.WriteLine(arg);
+                }
+                flog.WriteLine(ex);
             }
         }
     }
---
+++ TestProject/ProgramTest.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using GitLineTrim;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject
+{
+    /// <summary>
+    ///This is a test class for ProgramTest and is intended
+    ///to contain all ProgramTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class ProgramTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        #region Additional test attributes
+
+        //
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+
+        #endregion
+
+        /// <summary>
+        ///A test for Main
+        ///</summary>
+        [TestMethod()]
+        [DeploymentItem("GitLineTrim.exe")]
+        public void MainTest()
+        {
+            string[] newLines = {"\r\n", "\n", "\r"};
+            Dictionary<string, MemoryStream> fmem = new Dictionary<string, MemoryStream>();
+            Dictionary<Encoding, byte[]> codepages = new Dictionary<Encoding, byte[]>();
+            codepages.Add(Encoding.Unicode, new byte[] {0xFF, 0xFE});
+            codepages.Add(Encoding.BigEndianUnicode, new byte[] {0xFE, 0xFF});
+            codepages.Add(Encoding.GetEncoding("UTF-32BE"), new byte[] {0x00, 0x00, 0xFE, 0xFF});
+            codepages.Add(Encoding.UTF32, new byte[] {0xFF, 0xFE, 0x00, 0x00});
+            codepages.Add(Encoding.UTF8, new byte[] {0xEF, 0xBB, 0xBF});
+            codepages.Add(Encoding.Default, new byte[] {});
+
+            foreach (KeyValuePair<Encoding, byte[]> cdpg in codepages) {
+                for (int nl = 0; nl < newLines.Length; nl++) {
+                    string txtName = "Test_" + cdpg.Key.EncodingName + "_" + nl + ".txt";
+                    using (StreamWriter txt = new StreamWriter(File.Create(txtName), cdpg.Key)) {
+                        string[] lines = {"", "Русский текст и ♕ символ.   ", "  Hello  "};
+                        txt.NewLine = newLines[nl];
+                        fmem.Add(txtName, new MemoryStream());
+                        fmem[txtName].Write(cdpg.Value, 0, cdpg.Value.Length);
+                        foreach (string ln in lines) {
+                            txt.WriteLine(ln);
+                            byte[] bln = cdpg.Key.GetBytes(ln.TrimEnd() + txt.NewLine);
+                            fmem[txtName].Write(bln, 0, bln.Length);
+                        }
+                    }
+                    using (Stream fresult = File.OpenWrite("NeedResult" + txtName)) {
+                        fmem[txtName].WriteTo(fresult);
+                    }
+                }
+            }
+
+            using (BinaryWriter bin = new BinaryWriter(File.Create("Test_Binary.txt"))) {
+                fmem.Add("Test_Binary.txt", new MemoryStream());
+                byte[] binbytes = new byte[] {0xAA, 0xBB, 0x00, 0xCC};
+                for (int x = 0; x < 34764; x++) {
+                    bin.Write(binbytes, 0, binbytes.Length);
+                    fmem["Test_Binary.txt"].Write(binbytes, 0, binbytes.Length);
+                }
+            }
+            foreach (KeyValuePair<string, MemoryStream> file in fmem) {
+                Program_Accessor.Main(new string[] {file.Key, "-nobackup"});
+                using (BinaryReader bin = new BinaryReader(File.OpenRead(file.Key))) {
+                    Assert.IsFalse(bin.BaseStream.Length != file.Value.Length, "{0} is bad!", file.Key);
+                    file.Value.Seek(0, SeekOrigin.Begin);
+                    for (long x = 0; x < bin.BaseStream.Length; x++) {
+                        Assert.IsTrue(bin.ReadByte() == file.Value.ReadByte(), "{0} isn't identical in {1} byte",
+                                      file.Key, x);
+                    }
+                }
+            }
+        }
+    }
+}
