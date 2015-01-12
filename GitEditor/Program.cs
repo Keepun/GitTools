5e7ed85f5893c564a923df01861f99e2b6adfb81
Keepun <keepun@gmail.com>
2015-01-12 21:44:51 +0300

    GitEditor

---
+++ GitEditor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+
+namespace GitEditor
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0) {
+                Console.WriteLine("{0} \"path/Editor.exe\" [files]",
+                                  Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location));
+                return;
+            }
+            Console.WriteLine("GitEditor running...");
+            string path = args[0];
+            string pargs = "";
+            for (int x = 1; x < args.Length; x++) {
+                pargs += "\"" + args[x] + "\" ";
+            }
+            Console.WriteLine(path + " " + pargs);
+            Process.Start(path, pargs);
+            Console.WriteLine("continue?");
+            Console.ReadLine();
+        }
+    }
+}
